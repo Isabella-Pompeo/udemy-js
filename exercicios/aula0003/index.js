b3ec424fa6b10f86e75224c81029c02c972b970f
Isabella-Pompeo <isabellapompeo273@gmail.com>
2024-05-03 08:09:21 -0300

    j

---
+++ exercicios/aula0003/index.js
@@ -0,0 +1,9 @@
+console.log("Isabella 'Ramos'"); //String = Texto
+console.log('Isabella "Ramos"'); // Number = Número
+console.log(`'Isabella' "Ramos"`)
+
+//aspas simples dentro de aspas duplas
+//aspas duplas dentro de aspas simples
+//aspas duplas duplas e simples dentro de crase
+
+console.log(35, 15.85, 'Isabella Ramos de novo');
---
+++ exercicios/aula004/comentario.js
@@ -0,0 +1,9 @@
+// Escrever um comentario longo ( duas barras são usadas apenas para comentarios de uma linha)
+/*
+    comentario ( )
+    em bloco
+*/
+
+console.log('Ola, Mundo!');
+//console.log('Linha 1');
+console.log('Linha 2');
