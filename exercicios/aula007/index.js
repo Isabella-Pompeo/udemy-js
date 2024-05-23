45d409793cd75064dbd6a82353dabab6ea2f8a61
Isabella Ramos Pompeo <isabellapompeo273@gmail.com>
2024-05-22 23:12:37 -0300

    update index.js

---
+++ exercicios/aula007/index.js
@@ -0,0 +1,21 @@
+// Não podemos criar constantes com palavras reservadas
+// Constantes precisam ter nomes significativos
+// Não pode começar o nome de uma constante com um número
+// Não pode conter espaços ou traços
+// Utilizamos camelCase
+//case-sensitive
+// Nao pode modificar o valor de uma constante.
+// NAO UTILIZE VAR, UTILIZE LET.
+
+
+
+// String = Text | Number = number
+const primeiroNumero = '7'
+const segundoNumero = 10
+const resultado = primeiroNumero * segundoNumero
+const resultadoDuplicado = resultado * 2
+let resultadoTriplicado = resultado * 3
+resultadoTriplicado = resultadoTriplicado + 5
+//console.log(resultadoTriplicado)
+//console.log( typeof primeiroNumero )
+console.log(primeiroNumero + segundoNumero)
