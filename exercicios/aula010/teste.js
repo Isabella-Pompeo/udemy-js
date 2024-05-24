a62ea3d44b63286811d2cee50aee85c518588a60
Isabella Ramos Pompeo <isabellapompeo273@gmail.com>
2024-05-24 14:48:19 -0300

    update teste.js

--- exercicios/aula010/teste.js
+++ exercicios/aula010/teste.js
@@ -30,3 +30,24 @@ console.log(contador++)
 // Incremento = ++
 // Decremento = --
 
+let contador = 1; //NAO FAÇA DESSE JEITO
+console.log(contador++);
+
+// QUANDO QUISER ADICIONAR MAIS DE UM VALOR:
+const passo = 2;
+let contador = 0;
+
+contador = contador + passo // contador += passo
+console.log(contador)
+contador = contador + passo //contador += passo
+console.log(contador)
+
+/*OPERADORES DE ATRIBUIÇÃO
+  **=
+*/
+
+let contador = 2
+contador *= 2;
+contador *= 2;
+contador *= 2;
+console.log(contador)
