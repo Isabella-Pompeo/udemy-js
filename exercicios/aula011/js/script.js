3d66ce0b7417c44c988f94521874de0877e68082
Isabella Ramos Pompeo <isabellapompeo273@gmail.com>
2024-05-25 10:02:51 -0300

    update 6

--- exercicios/aula011/js/script.js
+++ exercicios/aula011/js/script.js
@@ -1,3 +1,11 @@
+/*
 window.alert('Com a nossa mensagem');
 window.confirm('Deseja continuar esta ação?')
 window.prompt('Escreva seu nome:')
+
+
+let num1 = prompt('Digite um numero')
+let num2 = prompt('Digite outro numero')
+
+//Tudo que o usuario digitat no prompt vai ser considerado uma string
+*/
