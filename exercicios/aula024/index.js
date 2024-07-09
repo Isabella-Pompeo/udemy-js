40f169fb14b87875e452db81760c29de8ab45678
Isabella Ramos Pompeo <isabellapompeo273@gmail.com>
2024-07-09 12:49:25 -0300

    update

---
+++ exercicios/aula024/index.js
@@ -0,0 +1,34 @@
+/*
+// if pode ser usado sozinho
+// sempre que utilizo a palavra else. preciso de um if amtes
+// eu posso ter varios else ifs na checagem
+// so posso ter um else na checagem
+// podemos usar condiçoes sem else if, utilizando apenas if e else
+
+
+
+
+
+const hora = 50;
+
+if (hora >= 0 && hora <= 11) {
+  console.log('Bom dia');
+} else if (hora >= 12 && hora <= 17) {
+  console.log('Boa tarde')
+} else if (hora >= 18 && hora <= 23) {
+  console.log('Boa noite');
+} else {
+  console.log('Olá');
+}
+
+
+
+const tenhoGrana = NaN;
+
+if (tenhoGrana) {
+  console.log('Vou sair de casa');
+} else {
+  console.log('Não vou sair de casa');
+}
+
+*/
