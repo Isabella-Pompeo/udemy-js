cafb988a026f02f695299bbb4b0806a1179489c2
Isabella Ramos Pompeo <isabellapompeo273@gmail.com>
2024-07-09 15:19:19 -0300

    update index.js

--- exercicios/aula025/index.js
+++ exercicios/aula025/index.js
@@ -1,1 +1,10 @@
 const numero = 10
+
+// Se (numero >= 0 && numero <= 5) ocorrer, faça isso {código}
+// Se não {o código}
+
+if (numero >= 0 && numero <= 5) {
+  console.log('O número está entre 0 e 5');
+} else {
+  console.log('O número NÃO está entre 0 e 5');
+}
