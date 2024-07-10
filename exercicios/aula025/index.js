629fe2681b06fc9b2f2bbdb29acc6ebcda7b5df8
Isabella Ramos Pompeo <isabellapompeo273@gmail.com>
2024-07-09 21:25:52 -0300

    update

--- exercicios/aula025/index.js
+++ exercicios/aula025/index.js
@@ -5,6 +5,12 @@ const numero = 10
 
 if (numero >= 0 && numero <= 5) {
   console.log('O número está entre 0 e 5');
+} else if (1 === 1){ // verdsdeira
+  console.log('LITERAL')
+} else if (numero >= 6 && numero <= 11){ // verdsdeira
+  console.log('O número está entre 6 e 11');
 } else {
-  console.log('O número NÃO está entre 0 e 5');
+  console.log('O número não está entre 0 e 11')
 }
+
+console.log('...Aqui vai o resto do código')
