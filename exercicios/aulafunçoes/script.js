6a4a622a34b414a222e573a43c7c0ea9f5bea684
Isabella Ramos Pompeo <isabellapompeo273@gmail.com>
2025-03-28 11:57:54 -0300

    update

---
+++ exercicios/aulafunçoes/script.js
@@ -0,0 +1,51 @@
+//Funçao void(vazia)
+//Funçao com parametros(s)
+//Funçao return
+//Funçao Arrow/ Arrow function
+
+let valor = 20
+
+/*
+function incrementa(){
+  valor = 20 + 30
+}
+
+incrementa()
+
+*/
+
+/*
+function mostraNome(nomedapessoa) {
+  
+  console.log(nomedapessoa)
+}
+
+mostraNome("Rodolfo")
+*/
+
+/*
+function soma(numero1, numero2){
+  
+  const somaDosNumeros = numero1 + numero2
+  
+  return somaDosNumeros
+}
+
+const meusNumeros1 = soma(10, 4)
+const meusNumeros2 = soma(2, 5)
+
+console.log(meusNumeros1)
+console.log(meusNumeros2)
+
+*/
+
+/*
+const multi = (numero1, numero2) => numero1 * numero2
+
+const multiplicacao = multi(2, 5)
+console.log(multiplicacao)
+
+*/
+
+
+
