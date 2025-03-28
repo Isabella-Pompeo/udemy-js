ccc71f57bdb261a5d1c0677fe600451022812551
Isabella Pompeo <162395260+Isabella-Pompeo@users.noreply.github.com>
2025-03-28 16:40:50 -0300

    ajuste5

--- exercicios/aula013/aula13.js
+++ exercicios/aula013/aula13.js
@@ -54,6 +54,7 @@ console.log(umaString.slice(umaString.length - 5, umaString.length - 1))
 */
 let umaString = "O rato roeu a roupa do rei de roma";
 
+
 console.log(umaString.replace(/r/g, 'l'))
 
 console.log(umaString.substring(umaString.length - 5, umaString.length - 1)) 
